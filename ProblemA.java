6b25a867fe6297fe9846f820991fff6de4a374b3
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-04-05 02:02:07 +0530

    Initial Commit

---
+++ ProblemA.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ProblemA {
+    public static void main(String[] args) {
+        Scanner hr = new Scanner(System.in);
+        String s = hr.nextLine();
+        System.out.println("Hello, " +s);
+
+        hr.close();
+    }
+}
